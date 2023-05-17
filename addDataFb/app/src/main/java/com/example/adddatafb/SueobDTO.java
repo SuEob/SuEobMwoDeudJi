0eecae6f157925cf3baecabd078cf580b807beb7
ChoiInHo123 <129516338+ChoiInHo123@users.noreply.github.com>
2023-05-18 00:01:38 +0900

    fabe0517777
    Post쪽 하는 중

---
+++ addDataFb/app/src/main/java/com/example/adddatafb/SueobDTO.java
@@ -0,0 +1,27 @@
+package com.example.adddatafb;
+
+public class SueobDTO {
+    String sueobName, teacherName, day;
+    int time;
+
+    public String getSueobName() {
+        return sueobName;
+    }
+    public void setSueobName(String asd) { sueobName = asd; }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+    public void setTeacherName(String asd) { teacherName = asd; }
+
+    public String getDay() {
+        return day;
+    }
+    public void setDay(String asd) { day = asd; }
+
+    public int getTime() {
+        return time;
+    }
+    public void setTime(int asd) { time = asd; }
+
+}
